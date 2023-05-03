7ea54ddd1f138991f0e1650d55395e1cbfe0f4aa
François Devantay <francois.devantay@studentfr.ch>
2023-05-03 14:51:37 +0200

    fix the prime style and start the complete integration with nodes

--- script.js
+++ script.js
@@ -41,3 +41,10 @@ function listen() {
     }
   };
 }
+
+const bubble = document.querySelector(".bubble");
+const outputArea = document.querySelector(".output-area");
+
+outputArea.addEventListener("mouseover", () => {
+  bubble.classList.toggle("writting");
+});
