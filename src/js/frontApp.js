93fca7f9b138d7daf00a5cb11a60eb9072b7ae3f
François Devantay <francois.devantay@studentfr.ch>
2023-05-04 15:43:57 +0200

    Revert "add the starterkit and node.js to project"
    This reverts commit e0206d4c48dc9a31692c6101fae849623b8c98d9.

--- src/js/frontApp.js
+++
@@ -1,6 +0,0 @@
-const bubble = document.querySelector(".bubble");
-const outputArea = document.querySelector(".output-area");
-
-outputArea.addEventListener("mouseover", () => {
-  bubble.classList.toggle("writting");
-});
